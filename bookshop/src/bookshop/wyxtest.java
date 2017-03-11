cfe85684104819e4a412dfc5991573c3e4ad370f
Administrator <Administrator@MS-20161105AUJD.mshome.net>
2017-03-11 14:06:22 +0800

    wyx new git

---
+++ bookshop/src/bookshop/wyxtest.java
@@ -0,0 +1,12 @@
+package bookshop;
+
+public class wyxtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is a new git");
+
+	}
+
+}
